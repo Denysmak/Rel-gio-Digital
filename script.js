044eb893d77bcd1b2f714a6b1b42f7e284a5930a
Denysmak <denysmakene13@gmail.com>
2023-05-26 13:31:10 -0300

    adiciona animação e icone

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+let relogio = document.querySelector('.relogio');
 let horas = document.querySelector('#horas');
 let minutos = document.querySelector('#minutos');
 let segundos = document.querySelector('#segundos');
 let zerozero = (a) => {
    return  a < 10 ? '0' + a : a;
 }
+setTimeout(() => {
+    relogio.style.display = 'flex'
+    relogio.style.animationName = 'slidein'
+}, 1000)
 setInterval(() => {
     horas.innerText = zerozero(new Date().getHours());
     minutos.innerText = zerozero(new Date().getMinutes());
     segundos.innerText = zerozero(new Date().getSeconds());
 }, 1000)
+
+
+
+
+
+
+
+
+
 
 
